876aac7362c2e3b7ebf1049ac7db566b0d45b72d
craig95 <u15029779@tuks.co.za>
2017-10-10 22:06:28 +0200

    Added new website content

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function solve(cnf) {
+
+}
+
+function solve1() {
+    var answer = solve("p cnf 4 2\n4 1 -3 0\n4 2 -3 0");
+}
+
+function solve2() {
+    var answer = solve("p cnf 4 4\n4 1 0\n2 0\n3 0\n3 -1 0");
+}
+
+function solve3() {
+    var answer = solve("p cnf 4 5\n1 0\n-1 0\n2 0\n3 0\n4 0")
+}
+
